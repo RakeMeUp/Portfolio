63d3e8db9775d681751fb9be6f8e454bf7fffbc0
Rake <bencehetes@gmail.com>
2021-11-03 10:52:51 +0100

    Hero WiP

---
+++ src/components/hero.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Hero() {
+  return (
+    <article className="bg-gray-700 h-screen flex justify-center">
+      <div className="bg-gray-900 max-h-10 text-gray-300 mt-14 lg:text-6xl text-2xl">
+        HERO
+      </div>
+    </article>
+  );
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app.jsx";
+import Hero from "./components/hero.jsx";
 import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Hero />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
